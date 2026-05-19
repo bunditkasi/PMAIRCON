5c04ac1265405ab65daaf8210a1b81fbd9c7a40b
bunditkasi <bundit.kasi@gmail.com>
2026-05-19 08:43:35 +0700

    test: align pm service test deps

--- tests/unit/pm-service.test.ts
+++ tests/unit/pm-service.test.ts
@@ -11,6 +11,7 @@ describe("savePmLog", () => {
         createPmLog: async (payload) => {
           writes.push(payload);
         },
+        deletePmLog: async () => {},
         updateUnitLatestPmDate: async () => {},
       },
       {
